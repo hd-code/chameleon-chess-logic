366e5d8a50282b5a63b32fe5f3cb284f1a4ab335
Hannes Dröse <hd@HDs-MacBook-Pro.local>
2019-11-08 15:04:11 +0100

    initial commit: changes pending

--- test.js
+++ test.js
@@ -5,5 +5,5 @@ let gs = ccl.initGame({0: true, 1: true, 2: true, 3: true})
 // gs = ccl.letAIadvanceGame(gs)
 do {
     console.log('Pawns:', gs.pawns.length, 'Turn:', gs.whoseTurn);
-    gs = ccl.letAIadvanceGame(gs)
+    gs = ccl.letComputerAdvanceGame(gs)
 } while(!ccl.isGameOver(gs))
